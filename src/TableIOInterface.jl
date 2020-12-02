eeddc790352aa0fe8e089f9ec407934c1baced8d
Benjamin Lungwitz <benj.lung@googlemail.com>
2020-12-02 13:57:29 +0100

    updated Pluto code for relative paths

--- src/TableIOInterface.jl
+++ src/TableIOInterface.jl
@@ -70,7 +70,7 @@ function get_example_code(t::ExcelFormat, directory:: AbstractString, filename::
     df_$(_get_varname(filename)) = let
         $import_table_io
         $import_dataframes
-        df_$(_get_varname(filename)) = DataFrames.DataFrame(TableIO.read_table($(_get_path_code(directory, filename)),
+        DataFrames.DataFrame(TableIO.read_table($(_get_path_code(directory, filename)),
             # "Sheet1", # uncomment this to specify the sheet name to be imported
             ); copycols=$(require_copycols(t)))
     end"""
@@ -81,7 +81,7 @@ function get_example_code(t::SQLiteFormat, directory:: AbstractString, filename:
     df_$(_get_varname(filename)) = let
         $import_table_io
         $import_dataframes
-        df_$(_get_varname(filename)) = DataFrames.DataFrame(TableIO.read_table($(_get_path_code(directory, filename)),
+        DataFrames.DataFrame(TableIO.read_table($(_get_path_code(directory, filename)),
             "tablename", # define the table name here
             ); copycols=$(require_copycols(t)))
     end"""
@@ -92,7 +92,7 @@ function get_example_code(t::ZippedFormat, directory:: AbstractString, filename:
     df_$(_get_varname(filename)) = let
         $import_table_io
         $import_dataframes
-        df_$(_get_varname(filename)) = DataFrames.DataFrame(TableIO.read_table($(_get_path_code(directory, filename)),
+        DataFrames.DataFrame(TableIO.read_table($(_get_path_code(directory, filename)),
             # "filename_in_archive", # uncomment this to specify a specific file name in the archive
             ); copycols=$(require_copycols(t)))
     end"""
@@ -103,13 +103,16 @@ function get_example_code(t::HDF5Format, directory:: AbstractString, filename:: 
     df_$(_get_varname(filename)) = let
         $import_table_io
         $import_dataframes
-        df_$(_get_varname(filename)) = DataFrames.DataFrame(TableIO.read_table($(_get_path_code(directory, filename)),
+        DataFrames.DataFrame(TableIO.read_table($(_get_path_code(directory, filename)),
             "/data", # define the key of the dataset in the HDF file here
             ); copycols=$(require_copycols(t)))
     end"""
 end
 
-_get_path_code(directory, filename) = """joinpath(dirname(@__FILE__), "$directory", "$filename")"""
+# `directory` is not used anymore because it corresponds to the notebook directory when inserting the data,
+# not the current notebook directory. 
+_get_path_code(directory, filename) = """joinpath(split(@__FILE__, '#')[1] * ".assets", "$filename")"""
+
 _get_varname(filename) = replace(filename, r"[\"\-,\.#@!\%\s+\;()\$&*\[\]\{\}'^]" => "_")
 
 get_example_code(directory:: AbstractString, filename:: AbstractString, args...; kwargs...) = get_example_code(get_file_type(filename), directory, filename, args...; kwargs...)
--- test/runtests.jl
+++ test/runtests.jl
@@ -23,31 +23,31 @@ using Test
             """df_test_csv = let
                 import TableIO
                 import DataFrames
-                DataFrames.DataFrame(TableIO.read_table(joinpath(dirname(@__FILE__), "my_notebook.assets", "test.csv")); copycols=false)
+                DataFrames.DataFrame(TableIO.read_table(joinpath(split(@__FILE__, '#')[1] * ".assets", "test.csv")); copycols=false)
             end""",
 
             """df_test_jdf = let
                 import TableIO
                 import DataFrames
-                DataFrames.DataFrame(TableIO.read_table(joinpath(dirname(@__FILE__), "my_notebook.assets", "test.jdf")); copycols=false)
+                DataFrames.DataFrame(TableIO.read_table(joinpath(split(@__FILE__, '#')[1] * ".assets", "test.jdf")); copycols=false)
             end""",
             
             """df_test_parquet = let
                 import TableIO
                 import DataFrames
-                DataFrames.DataFrame(TableIO.read_table(joinpath(dirname(@__FILE__), "my_notebook.assets", "test.parquet")); copycols=false)
+                DataFrames.DataFrame(TableIO.read_table(joinpath(split(@__FILE__, '#')[1] * ".assets", "test.parquet")); copycols=false)
             end""",    
 
             """df_test_arrow = let
                 import TableIO
                 import DataFrames
-                DataFrames.DataFrame(TableIO.read_table(joinpath(dirname(@__FILE__), "my_notebook.assets", "test.arrow")); copycols=false)
+                DataFrames.DataFrame(TableIO.read_table(joinpath(split(@__FILE__, '#')[1] * ".assets", "test.arrow")); copycols=false)
             end""",
             
             """df_test_xlsx = let
                 import TableIO
                 import DataFrames
-                df_test_xlsx = DataFrames.DataFrame(TableIO.read_table(joinpath(dirname(@__FILE__), "my_notebook.assets", "test.xlsx"),
+                DataFrames.DataFrame(TableIO.read_table(joinpath(split(@__FILE__, '#')[1] * ".assets", "test.xlsx"),
                     # "Sheet1", # uncomment this to specify the sheet name to be imported
                     ); copycols=false)
             end""",
@@ -55,13 +55,13 @@ using Test
             """df_test_json = let
                 import TableIO
                 import DataFrames
-                DataFrames.DataFrame(TableIO.read_table(joinpath(dirname(@__FILE__), "my_notebook.assets", "test.json")); copycols=true)
+                DataFrames.DataFrame(TableIO.read_table(joinpath(split(@__FILE__, '#')[1] * ".assets", "test.json")); copycols=true)
             end""",
             
             """df_test_zip = let
                 import TableIO
                 import DataFrames
-                df_test_zip = DataFrames.DataFrame(TableIO.read_table(joinpath(dirname(@__FILE__), "my_notebook.assets", "test.zip"),
+                DataFrames.DataFrame(TableIO.read_table(joinpath(split(@__FILE__, '#')[1] * ".assets", "test.zip"),
                     # "filename_in_archive", # uncomment this to specify a specific file name in the archive
                     ); copycols=false)
             end""",
@@ -69,7 +69,7 @@ using Test
             """df_test_sqlite = let
                 import TableIO
                 import DataFrames
-                df_test_sqlite = DataFrames.DataFrame(TableIO.read_table(joinpath(dirname(@__FILE__), "my_notebook.assets", "test.sqlite"),
+                DataFrames.DataFrame(TableIO.read_table(joinpath(split(@__FILE__, '#')[1] * ".assets", "test.sqlite"),
                     "tablename", # define the table name here
                     ); copycols=false)
             end""",
@@ -77,7 +77,7 @@ using Test
             """df_test_hdf = let
                 import TableIO
                 import DataFrames
-                df_test_hdf = DataFrames.DataFrame(TableIO.read_table(joinpath(dirname(@__FILE__), "my_notebook.assets", "test.hdf"),
+                DataFrames.DataFrame(TableIO.read_table(joinpath(split(@__FILE__, '#')[1] * ".assets", "test.hdf"),
                     "/data", # define the key of the dataset in the HDF file here
                     ); copycols=false)
             end""",
@@ -85,9 +85,9 @@ using Test
         ]
 
         for (i, fname) ∈ enumerate(fnames)
-            sample_code = get_example_code("my_notebook.assets", fname)
+            sample_code = get_example_code("my_notebook.jl.assets", fname)
             @test sample_code == expected_sample_code[i]
-            #println(sample_code)
+            println(sample_code)
         end
 
         @test is_extension_supported("csv") && !is_extension_supported("foo") && is_extension_supported("xlsx")
